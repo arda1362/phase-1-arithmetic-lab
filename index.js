f240f1184b34ba7ab2061402d3f466b60a3c4cfb
Ardavan Zareirad <ardavan2525@gmail.com>
2023-04-02 15:53:59 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+{let num1 = 2;
+     let num2 = 31;
+    let multiply = num1 * num2;
+}
+{ let random = 10;
+    Math.floor(Math.random() * 10) + 1;
+}
+{ let num3 = 8;
+let num4 = 2;
+let mod = 8/2
+}
+let max = (1, 2, 3, 4, 5, 20);
 
-// Write your code here
